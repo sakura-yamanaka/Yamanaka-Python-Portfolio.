45754c225ee747a685cd4977ab99f5aa165b2e51
sakura-yamanaka <syamanak@nd.edu>
2025-02-09 14:14:14 -0500

    Update main.py

--- basic_streamlit_app/main.py
+++ basic_streamlit_app/main.py
@@ -1,12 +1,11 @@
 import streamlit as st
 import pandas as pd
-import os
 
 st.write("Hello homebuyers!")
 st.title("Tallahassee Homes Explorer")
 st.write("Find your dream home with this interactive app! Explore homes that fit your budget, ideal square footage, preferred bed/bath count, and zip code.")
 
-df = pd.read_csv(os.path.abspath("./Yamanaka-Python-Portfolio./basic_streamlit_app/data/zillow.csv"))
+df = pd.read_csv("basic_streamlit_app/data/zillow.csv")
 
 zipcode = st.selectbox("Select a Zipcode", df["Zip"].unique())
 sqft = st.slider("Select Square Footage", min_value=int(df["Living Space (sq ft)"].min()), max_value=int(df["Living Space (sq ft)"].max()), value=int(df["Living Space (sq ft)"].mean()), step=100)
