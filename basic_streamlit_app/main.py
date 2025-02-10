25c12aa451c53809d0216ef61c1755d7dc100af5
sakura-yamanaka <syamanak@nd.edu>
2025-02-09 20:00:14 -0500

    Update main.py
    Here, I made several updates to my main.py app code to enhance the user experience. I introduced a second filter (filtered_df2) that suggests homes slightly different from a buyer’s exact preferences but still similar, helping them discover more potential options. Additionally, I changed "Baths" from floats to integers.
    
    Note: My original GitHub local files disappeared from my computer while reorganizing some files. As a result, I had to start over and create a new basic_streamlit_app folder and files. Fortunately, I had copied and pasted my original main.py. However, this explains why I don’t have many commit comments, my previous history was lost along with the deleted files.

--- basic_streamlit_app/main.py
+++ basic_streamlit_app/main.py
@@ -5,12 +5,13 @@ st.write("Hello homebuyers!")
 st.title("Tallahassee Homes Explorer")
 st.write("Find your dream home with this interactive app! Explore homes that fit your budget, ideal square footage, preferred bed/bath count, and zip code.")
 
-df = pd.read_csv("basic_streamlit_app/data/zillow.csv")
+import os
+df = pd.read_csv(os.path.abspath("data/zillow.csv"))
 
 zipcode = st.selectbox("Select a Zipcode", df["Zip"].unique())
 sqft = st.slider("Select Square Footage", min_value=int(df["Living Space (sq ft)"].min()), max_value=int(df["Living Space (sq ft)"].max()), value=int(df["Living Space (sq ft)"].mean()), step=100)
 beds = st.slider("Select Number of Bedrooms", min_value=int(df["Beds"].min()), max_value=int(df["Beds"].max()), value=int(df["Beds"].mean()), step=1)
-baths = st.slider("Select Number of Bathrooms", min_value=float(df["Baths"].min()), max_value=float(df["Baths"].max()), value=float(df["Baths"].mean()), step=0.5)
+baths = st.slider("Select Number of Bathrooms", min_value=int(df["Baths"].min()), max_value=int(df["Baths"].max()), value=int(df["Baths"].mean()), step=1)
 price = st.slider("Select List Price", min_value=int(df["List Price ($)"].min()), max_value=int(df["List Price ($)"].max()), value=int(df["List Price ($)"].mean()), step=5000)
 
 filtered_df = df[
@@ -21,5 +22,15 @@ filtered_df = df[
     (df["List Price ($)"] >= price - 5000) & (df["List Price ($)"] <= price + 5000)
 ]
 
+filtered_df2 = df[
+    (df["Living Space (sq ft)"] >= sqft - 500) & (df["Living Space (sq ft)"] <= sqft + 500) &
+    (df["Beds"] <= beds) &
+    (df["Baths"] <= baths) &
+    (df["List Price ($)"] >= price - 10000) & (df["List Price ($)"] <= price + 10000)
+]
+
 st.write(f"Homes that would be perfect for you:")
+st.dataframe(filtered_df)
+
+st.write(f"Homes that are (almost!) perfect for you:")
 st.dataframe(filtered_df)
